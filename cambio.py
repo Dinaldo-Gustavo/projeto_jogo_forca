03a76abd2ff7efffacddc95f778ec802385bf345
Dinaldo <gustavorocharibeiros@gmail.com>
2025-09-22 17:19:32 -0300

    Projeto de conversão de real para dólar e vice-versa

--- cambio.py
+++ cambio.py
@@ -1,10 +1,15 @@
 taxa = 5.33
 
+
 def real_para_dolar(real):
     return real / taxa
 
+
 def dolar_para_real(dolar):
+
     return taxa * dolar
+
+
 while True:
 
     escolha = int(input("Você deseja converter R$ para U$(1) ou U$ para R$(2) (-1 para encerrar)? "))
@@ -17,15 +22,12 @@ while True:
         print("Entrada Inválida!")
         print("Tente novamente")
 
-
     elif escolha == 1:
-        real = float(input("Digite o valor em Reais: R$"))
-        retorno = real_para_dolar(real)
-        print(f"R${real} equivalem a US${retorno:.2f}")
+        reais = float(input("Digite o valor em Reais: R$"))
+        retorno = real_para_dolar(reais)
+        print(f"R${reais} equivalem a US${retorno:.2f}")
 
     elif escolha == 2:
-        dolar = float(input("Digite o valor em Dólares: U$"))
-        retorno = dolar_para_real(dolar)
-        print(f"U${dolar} equivalem a R${retorno:.2f}")
-
-
+        dolares = float(input("Digite o valor em Dólares: U$"))
+        retorno = dolar_para_real(dolares)
+        print(f"U${dolares} equivalem a R${retorno:.2f}")
